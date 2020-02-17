c5f38059079334a283ba320bee694f782bcad526
Damian <18230253@studentmail.ul.ie>
2020-02-17 22:28:33 +0000

    Added fullscreen, checkTrains activity, button & onClickListener with intent to launch checkTrains activity

---
+++ IrishRailTrainTracker/app/src/main/java/com/example/traintracker/CheckTrains.java
@@ -0,0 +1,20 @@
+package com.example.traintracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheckTrains extends Activity {
+
+    TextView trainsPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_trains_relative);
+
+        trainsPage = (TextView) findViewById(R.id.trainsPage);
+
+    }
+}
--- IrishRailTrainTracker/app/src/main/java/com/example/traintracker/MainActivity.java
+++ IrishRailTrainTracker/app/src/main/java/com/example/traintracker/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.traintracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -15,6 +18,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_relative);
 
+
         checkTrains = (Button) findViewById(R.id.checkTrains);
+
+        checkTrains.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent newIntent = new Intent(getApplicationContext(), CheckTrains.class);
+                startActivity(newIntent);
+            }
+        });
     }
 }
