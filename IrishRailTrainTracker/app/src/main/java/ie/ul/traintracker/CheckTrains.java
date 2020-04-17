22aaf7455a6af0c04f524f92a5410b60671575e4
Damian-19 <47003214+Damian-19@users.noreply.github.com>
2020-04-17 14:40:15 +0100

    Added notification scheduling

--- IrishRailTrainTracker/app/src/main/java/ie/ul/traintracker/CheckTrains.java
+++ IrishRailTrainTracker/app/src/main/java/ie/ul/traintracker/CheckTrains.java
@@ -1,25 +1,43 @@
 package ie.ul.traintracker;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+
 public class CheckTrains extends Activity {
 
-    private static final String CHANNEL_ID = "test_channel";
+    public static final String CHANNEL_ID = "1001";
+    private static final String EXTRA_NOTIFICATION_ID = "2";
+
     TextView trainsPage;
-    Button notificationButton;
+    Button notificationButton, btnDate;
+    final Calendar myCalendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +48,7 @@ public class CheckTrains extends Activity {
         createNotificationChannel();
         trainsPage = (TextView) findViewById(R.id.trainsPage);
         notificationButton = (Button) findViewById(R.id.notification_button);
+        btnDate = (Button) findViewById(R.id.btnDate);
 
         notificationButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,12 +58,55 @@ public class CheckTrains extends Activity {
         });
 
     }
+    private void scheduleNotification (Notification notification , long delay) {
+        Intent notificationIntent = new Intent(this, AlarmReceiver.class ) ;
+        notificationIntent.putExtra(AlarmReceiver. NOTIFICATION_ID , 1 ) ;
+        notificationIntent.putExtra(AlarmReceiver. NOTIFICATION , notification) ;
+        PendingIntent pendingIntent = PendingIntent. getBroadcast ( this, 0 , notificationIntent , PendingIntent. FLAG_UPDATE_CURRENT ) ;
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context. ALARM_SERVICE ) ;
+        assert alarmManager != null;
+        alarmManager.set(AlarmManager. ELAPSED_REALTIME_WAKEUP , delay , pendingIntent) ;
+    }
+    private Notification getNotification (String content) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder( this, EXTRA_NOTIFICATION_ID) ;
+        builder.setContentTitle( "You have a journey today" ) ;
+        builder.setContentText(content) ;
+        builder.setSmallIcon(R.drawable. ic_launcher_foreground ) ;
+        builder.setAutoCancel( true ) ;
+        builder.setChannelId(CHANNEL_ID ) ;
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        return builder.build() ;
+    }
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet (DatePicker view , int year , int monthOfYear , int dayOfMonth) {
+            myCalendar .set(Calendar. YEAR , year) ;
+            myCalendar .set(Calendar. MONTH , monthOfYear) ;
+            myCalendar .set(Calendar. DAY_OF_MONTH , dayOfMonth) ;
+            updateLabel() ;
+        }
+    } ;
+    public void setDate (View view) {
+        new DatePickerDialog(
+                CheckTrains.this, date ,
+                myCalendar .get(Calendar. YEAR ) ,
+                myCalendar .get(Calendar. MONTH ) ,
+                myCalendar .get(Calendar. DAY_OF_MONTH )
+        ).show() ;
+    }
+    private void updateLabel () {
+        String myFormat = "dd/MM/yy" ; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat , Locale. getDefault ()) ;
+        Date date = myCalendar .getTime() ;
+        btnDate .setText(sdf.format(date)) ;
+        scheduleNotification(getNotification( btnDate .getText().toString()) , date.getTime()) ;
+    }
 
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            int importance = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel channel = new NotificationChannel( CHANNEL_ID, name, importance);
             channel.setDescription(description);
 
@@ -68,7 +130,7 @@ public class CheckTrains extends Activity {
                 .setSmallIcon(R.drawable.logo)
                 .setContentTitle(testTitle)
                 .setContentText(testContent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setContentIntent(pendingIntent)
                 .setFullScreenIntent(fullScreenPendingIntent, true)
                 .setCategory(NotificationCompat.CATEGORY_REMINDER)
