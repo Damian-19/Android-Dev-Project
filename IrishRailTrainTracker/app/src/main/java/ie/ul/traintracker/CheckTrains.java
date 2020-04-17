6f0d5300834a1bb8bafd35f06e8fcb8597a4cd11
Damian-19 <47003214+Damian-19@users.noreply.github.com>
2020-04-17 13:01:22 +0100

    Added button to send test notification

--- IrishRailTrainTracker/app/src/main/java/ie/ul/traintracker/CheckTrains.java
+++ IrishRailTrainTracker/app/src/main/java/ie/ul/traintracker/CheckTrains.java
@@ -1,19 +1,80 @@
 package ie.ul.traintracker;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class CheckTrains extends Activity {
 
+    private static final String CHANNEL_ID = "test_channel";
     TextView trainsPage;
+    Button notificationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_trains_relative);
 
+        //Notification
+        createNotificationChannel();
         trainsPage = (TextView) findViewById(R.id.trainsPage);
+        notificationButton = (Button) findViewById(R.id.notification_button);
 
+        notificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
+
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel( CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            //Register channel with system
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void sendNotification() {
+        Intent fullScreenIntent = new Intent(this, CheckTrains.class);
+        PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(this, 0, fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent intent = new Intent(this, CheckTrains.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        CharSequence testTitle = "Test Title";
+        CharSequence testContent = "Test Content";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle(testTitle)
+                .setContentText(testContent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setFullScreenIntent(fullScreenPendingIntent, true)
+                .setCategory(NotificationCompat.CATEGORY_REMINDER)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
     }
 }
